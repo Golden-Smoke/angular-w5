55762b312d35114edd52a4aa848294a55b888e4c
Plamen <plamengeorgiev967@gmail.com>
2024-05-18 20:45:04 +0300

    Adding content to home page and footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PostListComponent } from './post-list/post-list.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,PostListComponent,NavbarComponent],
+  imports: [
+    RouterOutlet,
+    PostListComponent,
+    NavbarComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
